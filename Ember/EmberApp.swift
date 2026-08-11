10b2acfbd5a66bf8efa9978e2f9e2f8e5aa3ed14
Youngmin Cho <choyoungmin0322@gmail.com>
2026-08-11 10:23:51 +0900

    Initial Commit

---
+++ Ember/EmberApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmberApp.swift
+//  Ember
+//
+//  Created by Youngmin Cho on 8/11/26.
+//
+
+import SwiftUI
+
+@main
+struct EmberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
